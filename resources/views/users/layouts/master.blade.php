388f433c04ed555749dce63ca8b962e02b9683b8
Ghalmas Shanditya <ghalmasshandityaaa@gmail.com>
2021-09-23 11:57:46 +0700

    resource

--- resources/views/users/layouts/master.blade.php
+++ resources/views/users/layouts/master.blade.php
@@ -10,8 +10,8 @@
             @yield('title')
         </title>
         <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/dist/img/icons')}}/favicon.png">
-        {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous"> --}}
-        <link href="{{asset('assets/dist/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        {{-- <link href="{{asset('assets/dist/bootstrap')}}/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous"> --}}
         <!--======= GOOGLE FONTS ========-->
         <link rel="preconnect" href="https://fonts.gstatic.com/">
         <link href="https://fonts.googleapis.com/css?family=Quicksand:500,600,700&display=swap" rel="stylesheet">
@@ -48,11 +48,13 @@
     @include('users.layouts.footer')
     @include('sweetalert::alert')
 
-    <script src="{{ asset("assets/dist/bootstrap") }}/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    {{-- <script src="{{ asset("assets/dist/bootstrap") }}/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> --}}
     <script src="{{ asset("assets/dist/bootstrap") }}/js/form-validation.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>
-    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script> --}}
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
     </body>
 </html>
 
